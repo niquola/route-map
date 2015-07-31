075af2240ac06d9e33d1ee3bf583d99212fee9f5
niquola <niquola@gmail.com>
2015-07-31 15:10:25 +0000

    update project

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to route-map
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject route-map "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject route-map "0.0.1"
+  :description "clojure routing library, where route is written as hash-map"
+  :url "https://github.com/niquola/route-map"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:source-paths ["examples" "src"]
-                   :dependencies [[ring "1.2.2"]
-                                  [hiccup "1.0.5"]]}})
+                   :dependencies [[ring "1.2.2"] ]}})
 
