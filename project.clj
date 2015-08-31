05445fff78358a48aabfe162bb9b2e6c9cf3ee92
niquola <niquola@gmail.com>
2015-08-31 20:38:04 +0300

    Move to clj 1.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/niquola/route-map"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :repositories  [["clojars"  {:url "https://clojars.org/repo" :sign-releases false}]]
   :profiles {:dev {:source-paths ["examples" "src"]
                    :dependencies [[ring "1.2.2"] ]}})
