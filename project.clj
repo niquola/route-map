cd24f71263775a5d22557952447594c456a4cb55
niquola <niquola@gmail.com>
2016-10-20 13:14:12 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject route-map "0.0.3"
+(defproject route-map "0.0.4"
   :description "clojure routing library, where route is written as hash-map"
   :url "https://github.com/niquola/route-map"
   :license {:name "Eclipse Public License"
