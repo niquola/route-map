1e33ebcff4575e46bf2c8026e4cf259362c70fa5
niquola <niquola@gmail.com>
2016-10-20 13:17:00 +0300

    bump version

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :repositories  [["clojars"  {:url "https://clojars.org/repo" :sign-releases false}]]
+  :repositories  [["clojars"  {:url "https://clojars.org/repo"
+                               :sign-releases false}]]
   :profiles {:dev {:source-paths ["examples" "src"]
                    :dependencies [[ring "1.2.2"] ]}})
 
